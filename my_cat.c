06ea968aacfe613d32b6550ab86f4b23860ecf26
Cool <jason901214edaaa@gmail.com>
2025-10-16 16:04:24 +0800

    Improve robustness of file processing
    The previous implementation was not robust and had serveral issues.
    
    - Replaced printf with write() for binary-safe output, preventing
    potential buffer overflows.
    - Implemented a while loop around read() to support files larger than
      the buffer size.
    - Changed error handling in the multi-file loop from return to continue,
      allowing the program to process subsequent files after an error.

---
+++ my_cat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        printf("用法: %s <檔名>\n", argv[0]);
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        char *file = argv[i];
+        int fd = open(file, O_RDONLY);
+
+        if (fd == -1) {
+            perror("open");
+            continue; // If one of them fail, others can continue
+        }
+
+        char buf[BUFFER_SIZE];
+        ssize_t bytes_read; // ssize_t is read's return type
+
+        while ((bytes_read = read(fd, buf, BUFFER_SIZE)) > 0) {
+            if (write(1, buf, bytes_read) != bytes_read) {
+                perror("write error");
+                break;
+            }
+        }
+        
+        if (bytes_read == -1) {
+            perror("read 錯誤");
+        }
+        close(fd);
+    }
+    return 0;
+}
